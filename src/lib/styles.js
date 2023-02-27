7008396563893915b2873d35856fa0b2219d0f35
Cosqueto582 <gabsoueu01@gmail.com>
2023-02-27 15:15:17 -0300

    committ

--- src/lib/styles.js
+++ src/lib/styles.js
@@ -1,17 +1,14 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "gray",
-    fontSize: "90px",
-    
-  },
-  paragraph: {
-    fontSize: "90px",
-    fontWeight: "bold",
-  },
-  
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "gray",
+        fontSize: "90px",
+
+    },
+
+
 });
